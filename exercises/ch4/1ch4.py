d0c62e937f7f471b0937dd146fedf8e92feacf3e
goehmen <greg.oehmen@gmail.com>
2014-08-21 16:33:32 -0700

    python ch4 exercises

--- exercises/ch4/1ch4.py
+++ exercises/ch4/1ch4.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from swampy.TurtleWorld import *
+import math
 
 world = TurtleWorld()
 t = Turtle()
@@ -26,16 +27,29 @@ def polygon2(t,length,n):
     t.delay = 0.01
     for i in range(n):
         fd(t, length)
-        lt(t,(360/n))
+        lt(t,(360.0/n))
 
-polygon2(t,100,36)
+#polygon2(t,10,36)
 
-#def circle(t,r):
-#    for i in range(n):
-#        fd(t, length)
-#        lt(t,(360/n))
-#
-#polygon2(t,100,36)
+def circle(t,r):
+    t.delay = 0.01
+    length=3.0
+    c=(2.0*math.pi*r)
+#    c=(2*3*r)
+    n=c/length
+    polygon2(t,length,n)
+    
+circle(t,60.0)
+
+#def arc(t,r,angle):
+#    t.delay = 0.01
+#    length=3
+##    c=(2*math.pi*r)
+#    c=(2*3*r)
+#    n=c/length
+#    polygon2(t,length,n)
+#    
+#arc(t,60,#angle)
 
 wait_for_user()
     
